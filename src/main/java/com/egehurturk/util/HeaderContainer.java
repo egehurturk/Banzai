20cf37376fb79e12a7c167323e1125fd31273988
Ege Hurturk <ege.hurturk@gmail.com>
2021-01-03 16:58:35 +0300

    Accepted headers here

--- src/main/java/com/egehurturk/util/HeaderContainer.java
+++ src/main/java/com/egehurturk/util/HeaderContainer.java
@@ -4,5 +4,15 @@ package com.egehurturk.util;
  * Stores identified headers
  */
 public class HeaderContainer {
-
+    public final String connection = "Connection: ";
+    public final String accept = "Accept: ";
+    public final String contentType = "Content-Type: ";
+    public final String acceptEncoding = "Accept-Encoding: ";
+    public final String contentEncoding = "Content-Encoding: ";
+    public final String acceptLanguage = "Accept-Language: ";
+    public final String contentLanguage = "Content-Language: ";
+    public final String server = "Server: ";
+    public final String date = "Date: ";
+    public final String contentLength = "Content-Length: ";
+    public final String host = "Host: ";
 }
