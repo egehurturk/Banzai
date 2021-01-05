88d0bbde57413e60d12291032223d0bce05d796c
Ege Hurturk <ege.hurturk@gmail.com>
2021-01-05 23:05:48 +0300

    Ticking headers I'm done with

--- src/main/java/com/egehurturk/util/HeaderContainer.java
+++ src/main/java/com/egehurturk/util/HeaderContainer.java
@@ -6,13 +6,13 @@ package com.egehurturk.util;
 public class HeaderContainer {
     public final String connection = "Connection: ";
     public final String accept = "Accept: ";
-    public final String contentType = "Content-Type: ";
+    public final String contentType = "Content-Type: "; // [X]
     public final String acceptEncoding = "Accept-Encoding: ";
     public final String contentEncoding = "Content-Encoding: ";
-    public final String acceptLanguage = "Accept-Language: ";
-    public final String contentLanguage = "Content-Language: ";
-    public final String server = "Server: ";
-    public final String date = "Date: ";
-    public final String contentLength = "Content-Length: ";
-    public final String host = "Host: ";
+    public final String acceptLanguage = "Accept-Language: ";   // [X]
+    public final String contentLanguage = "Content-Language: "; //[X]
+    public final String server = "Server: "; // [X]
+    public final String date = "Date: "; // [X]
+    public final String contentLength = "Content-Length: "; // [X]
+    public final String host = "Host: "; // [X]
 }
