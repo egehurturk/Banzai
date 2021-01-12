c6c890f6bd6c0edd783593861629ca1eb6e97a5b
Ege Hurturk <ege.hurturk@gmail.com>
2021-01-12 22:43:21 +0300

    Constants

--- src/main/java/com/egehurturk/util/HeaderEnum.java
+++ src/main/java/com/egehurturk/util/HeaderEnum.java
@@ -2,18 +2,18 @@ package com.egehurturk.util;
 
 public enum HeaderEnum {
 
-    CONNECTION("Connection: ", HeaderStatusEnum.General),
-    ACCEPT("ACCEPT", HeaderStatusEnum.Request),
-    ACCEPT_ENCODING("Accept-Encoding: ", HeaderStatusEnum.Request),
-    ACCEPT_LANGUAGE("Accept-Language: ", HeaderStatusEnum.Request),
-    HOST("Host", HeaderStatusEnum.Request),
-    SERVER("Server: ", HeaderStatusEnum.Response),
-    USER_AGENT("User-Agent: ", HeaderStatusEnum.Request),
-    DATE("Date: ", HeaderStatusEnum.Response),
-    CONTENT_TYPE("Content-Type: ", HeaderStatusEnum.General),
-    CONTENT_LENGTH("Content-Length: ", HeaderStatusEnum.General),
-    CONTENT_ENCODING("Content-Encoding: ", HeaderStatusEnum.General),
-    CONTENT_LANGUAGE("Content-Language: ", HeaderStatusEnum.General)
+    CONNECTION(Constants.CCONNECTION, HeaderStatusEnum.General),
+    ACCEPT(Constants.CACCEPT, HeaderStatusEnum.Request),
+    ACCEPT_ENCODING(Constants.CACCEPT_ENCODING, HeaderStatusEnum.Request),
+    ACCEPT_LANGUAGE(Constants.CACCEPT_LANGUAGE, HeaderStatusEnum.Request),
+    HOST(Constants.CHOST, HeaderStatusEnum.Request),
+    SERVER(Constants.CSERVER, HeaderStatusEnum.Response),
+    USER_AGENT(Constants.CUSER_AGENT, HeaderStatusEnum.Request),
+    DATE(Constants.CDATE, HeaderStatusEnum.Response),
+    CONTENT_TYPE(Constants.CCONTENT_TYPE, HeaderStatusEnum.General),
+    CONTENT_LENGTH(Constants.CCONTENT_LENGTH, HeaderStatusEnum.General),
+    CONTENT_ENCODING(Constants.CCONTENT_ENCODING, HeaderStatusEnum.General),
+    CONTENT_LANGUAGE(Constants.CCONTENT_LANGUAGE, HeaderStatusEnum.General)
     ;
 
     public final String NAME;
@@ -22,5 +22,20 @@ public enum HeaderEnum {
     HeaderEnum(String NAME, HeaderStatusEnum PLACE) {
         this.NAME = NAME;
         this.PLACE = PLACE;
+    }
+
+    private static class Constants {
+        public static final String CCONNECTION = "Connection: ";
+        public static final String CACCEPT = "Accept: ";
+        public static final String CACCEPT_LANGUAGE = "Accept-Language: ";
+        public static final String CACCEPT_ENCODING = "Accept-Encoding: ";
+        public static final String CHOST = "Host: ";
+        public static final String CSERVER = "Server: ";
+        public static final String CUSER_AGENT = "User-Agent: ";
+        public static final String CDATE = "Date: ";
+        public static final String CCONTENT_TYPE = "Content-Type: ";
+        public static final String CCONTENT_LENGTH = "Content-Length: ";
+        public static final String CCONTENT_ENCODING = "Content-Encoding: ";
+        public static final String CCONTENT_LANGUAGE = "Content-Language: ";
     }
 }
--- src/main/java/com/egehurturk/util/StatusEnum.java
+++ src/main/java/com/egehurturk/util/StatusEnum.java
@@ -1,14 +1,14 @@
 package com.egehurturk.util;
 
 public enum StatusEnum {
-    _100_CONTINUE(100, "Continue"),
-    _200_OK(200, "OK"),
-    _400_BAD_REQUEST(400, "Bad Request"),
-    _403_FORBIDDEN(403, "Forbidden"),
-    _404_NOT_FOUND(404, "Not Found"),
-    _405_METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
-    _500_INTERNAL_ERROR(500, "Internal Server Error"),
-    _501_NOT_IMPLEMENTED(501, "Not Implemented"),
+    _100_CONTINUE(Constants.C100, Constants.CM100),
+    _200_OK(Constants.C200, Constants.CM200),
+    _400_BAD_REQUEST(Constants.C400, Constants.CM400),
+    _403_FORBIDDEN(Constants.C403, Constants.CM403),
+    _404_NOT_FOUND(Constants.C404, Constants.CM404),
+    _405_METHOD_NOT_ALLOWED(Constants.C405, Constants.CM405),
+    _500_INTERNAL_ERROR(Constants.C500, Constants.CM500),
+    _501_NOT_IMPLEMENTED(Constants.C501, Constants.CM501),
 
     ;
 
@@ -18,5 +18,29 @@ public enum StatusEnum {
     StatusEnum(int StatCode, String Message) {
         this.STATUS_CODE = StatCode;
         this.MESSAGE = Message;
+    }
+
+    private static class Constants {
+        public static final int C100 = 100;
+        public static final int C200 = 200;
+        public static final int C400 = 400;
+        public static final int C403 = 403;
+        public static final int C404 = 404;
+        public static final int C405 = 405;
+        public static final int C500 = 500;
+        public static final int C501 = 501;
+
+
+        public static final String CM100 = "Continue";
+        public static final String CM200 = "OK";
+        public static final String CM400 = "Bad Request";
+        public static final String CM403 = "Forbidden";
+        public static final String CM404 = "Not Found";
+        public static final String CM405 = "Method Not Allowed";
+        public static final String CM500 = "Internal Server Error";
+        public static final String CM501 = "Not Implemented";
+
+
+
     }
 }
