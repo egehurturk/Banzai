4eb49f4d1a5d59b287d8dcefcd2756600568f7ce
Ege Hurturk <ege.hurturk@gmail.com>
2021-01-14 15:16:17 +0300

    Driver class debug

--- src/main/java/com/egehurturk/DriverClassForTest.java
+++ src/main/java/com/egehurturk/DriverClassForTest.java
@@ -7,8 +7,8 @@ public class DriverClassForTest {
     public static void main(String[] args) throws IOException {
         HttpServer tcpServer = new HttpServer();
         tcpServer.setConfigPropFile("src/main/resources/server.properties");
-        System.out.println(tcpServer.getConfigPropFile());
         tcpServer.configureServer();
+        System.out.println("[DEBUG][DEBUG] starting server from driver [DriverClassForTest/main]");
         tcpServer.start();
     }
 }
