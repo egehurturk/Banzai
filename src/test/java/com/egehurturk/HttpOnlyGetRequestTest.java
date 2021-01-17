44940e87f917506852e9ebb37ffdd8496b922a8d
Ege Hurturk <ege.hurturk@gmail.com>
2021-01-17 13:39:49 +0300

    @DisplayName annotation for convenience in tests

--- src/test/java/com/egehurturk/HttpOnlyGetRequestTest.java
+++ src/test/java/com/egehurturk/HttpOnlyGetRequestTest.java
@@ -4,7 +4,8 @@ package com.egehurturk;
 import com.egehurturk.exceptions.HttpRequestException;
 import com.egehurturk.lifecycle.HttpRequest;
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -14,6 +15,7 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@DisplayName("Http Request tests (%80 coverage)")
 public class HttpOnlyGetRequestTest {
 
 
@@ -62,6 +64,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderValidTestGetRequest")
+    @DisplayName("Test valid http request's method to be GET")
     public void testHttpRequestMethod(String http) throws IOException, HttpRequestException {
         testRequest = new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))));
         Assert.assertEquals(testRequest.method, "GET");
@@ -69,6 +72,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderValidTestGetRequest")
+    @DisplayName("Test valid http request's scheme to be HTTP/1.1")
     public void testHttpRequestScheme(String http) throws IOException, HttpRequestException {
         testRequest = new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))));
         Assert.assertEquals(testRequest.scheme, "HTTP/1.1");
@@ -76,6 +80,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderValidTestGetRequest")
+    @DisplayName("Test valid http request's path to be /")
     public void testHttpRequestPath(String http) throws IOException, HttpRequestException {
         testRequest = new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))));
         Assert.assertEquals(testRequest.path, "/");
@@ -83,6 +88,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderValidTestGetRequest")
+    @DisplayName("Test valid http request's header host to be localhost:8080")
     public void testHttpRequestHeaderHost(String http) throws IOException, HttpRequestException {
         testRequest = new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))));
         Assert.assertEquals(testRequest.headers.get("host").trim(), "localhost:8080");
@@ -90,6 +96,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderValidTestGetRequest")
+    @DisplayName("Test valid http request's header connection to be keep-alive")
     public void testHttpRequestHeaderConnection(String http) throws IOException, HttpRequestException {
         testRequest = new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))));
         Assert.assertEquals(testRequest.headers.get("connection").trim(), "keep-alive");
@@ -97,6 +104,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderValidTestGetRequest")
+    @DisplayName("Test valid http request's header user-agent to be mozilla")
     public void testHttpRequestHeaderUserAgent(String http) throws IOException, HttpRequestException {
         testRequest = new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))));
         Assert.assertEquals(testRequest.headers.get("user-agent").trim(), "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36".toLowerCase());
@@ -105,6 +113,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderValidTestGetRequest")
+    @DisplayName("Test valid http request's header body to be null (b/c it is GET)")
     public void testHttpRequestBodyToBeNullInGetRequest(String http) throws IOException, HttpRequestException {
         testRequest = new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))));
         Assert.assertNull(testRequest.body);
@@ -119,6 +128,7 @@ public class HttpOnlyGetRequestTest {
      */
     @ParameterizedTest
     @MethodSource("httpProviderInvalidTestRequestWithInvalidMethod")
+    @DisplayName("Test invalid http request's invalid method that asserts BadRequest400")
     public void testInvalidHttpRequestMethod(String http) throws IOException {
         assertThrows(com.egehurturk.exceptions.HttpRequestException.class,
                 () -> new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))))
@@ -127,6 +137,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderInvalidTestRequestWithInvalidMethod")
+    @DisplayName("Test invalid http request's invalid scheme that asserts BadRequest400")
     public void testInvalidHttpRequestScheme(String http) throws IOException {
         assertThrows(com.egehurturk.exceptions.HttpRequestException.class,
                 () -> new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))))
@@ -135,6 +146,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderInvalidTestRequestWithInvalidMethod")
+    @DisplayName("Test invalid http request's invalid path that asserts BadRequest400")
     public void testInvalidHttpRequestPath(String http) throws IOException {
         assertThrows(com.egehurturk.exceptions.HttpRequestException.class,
                 () -> new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))))
@@ -143,6 +155,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderInvalidTestRequestWithInvalidMethod")
+    @DisplayName("Test invalid http request's invalid header host that asserts BadRequest400")
     public void testInvalidHttpRequestHeaderHost(String http) throws IOException {
         assertThrows(com.egehurturk.exceptions.HttpRequestException.class,
                 () -> new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))))
@@ -151,6 +164,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderInvalidTestRequestWithInvalidMethod")
+    @DisplayName("Test invalid http request's invalid header connection that asserts BadRequest400")
     public void testInvalidHttpRequestHeaderConnection(String http) throws IOException {
         assertThrows(com.egehurturk.exceptions.HttpRequestException.class,
                 () -> new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))))
@@ -159,6 +173,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderInvalidTestRequestWithInvalidMethod")
+    @DisplayName("Test invalid http request's invalid header user-agent that asserts BadRequest400")
     public void testInvalidHttpRequestHeaderUserAgent(String http) throws IOException {
         assertThrows(com.egehurturk.exceptions.HttpRequestException.class,
                 () -> new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))))
@@ -168,6 +183,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderInvalidTestRequestWithInvalidMethod")
+    @DisplayName("Test invalid http request's invalid header body that asserts BadRequest400")
     public void testInvalidHttpRequestBodyToBeNullInGetRequest(String http) throws IOException {
         assertThrows(com.egehurturk.exceptions.HttpRequestException.class,
                 () -> new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))))
@@ -184,6 +200,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderInvalidTestRequestWithInvalidHttp")
+    @DisplayName("Test invalid http request's valid method (invalid scheme) that asserts BadRequest400")
     public void testInvalidHttpSchemeRequestMethod(String http) throws IOException {
         assertThrows(com.egehurturk.exceptions.HttpRequestException.class,
                 () -> new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))))
@@ -192,6 +209,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderInvalidTestRequestWithInvalidHttp")
+    @DisplayName("Test invalid http request's valid scheme that asserts BadRequest400")
     public void testInvalidHttpSchemeRequestScheme(String http) throws IOException {
         assertThrows(com.egehurturk.exceptions.HttpRequestException.class,
                 () -> new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))))
@@ -200,6 +218,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderInvalidTestRequestWithInvalidHttp")
+    @DisplayName("Test invalid http request's valid path (invalid scheme) that asserts BadRequest400")
     public void testInvalidHttpSchemeRequestPath(String http) throws IOException {
         assertThrows(com.egehurturk.exceptions.HttpRequestException.class,
                 () -> new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))))
@@ -208,6 +227,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderInvalidTestRequestWithInvalidHttp")
+    @DisplayName("Test invalid http request's valid header-host (invalid scheme) that asserts BadRequest400")
     public void testInvalidHttpSchemeRequestHeaderHost(String http) throws IOException {
         assertThrows(com.egehurturk.exceptions.HttpRequestException.class,
                 () -> new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))))
@@ -216,6 +236,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderInvalidTestRequestWithInvalidHttp")
+    @DisplayName("Test invalid http request's hedaer connection (invalid scheme) that asserts BadRequest400")
     public void testInvalidHttpSchemeRequestHeaderConnection(String http) throws IOException {
         assertThrows(com.egehurturk.exceptions.HttpRequestException.class,
                 () -> new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))))
@@ -224,6 +245,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderInvalidTestRequestWithInvalidHttp")
+    @DisplayName("Test invalid http request's header user-agent (invalid scheme) that asserts BadRequest400")
     public void testInvalidHttpSchemeRequestHeaderUserAgent(String http) throws IOException {
         assertThrows(com.egehurturk.exceptions.HttpRequestException.class,
                 () -> new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))))
@@ -233,6 +255,7 @@ public class HttpOnlyGetRequestTest {
 
     @ParameterizedTest
     @MethodSource("httpProviderInvalidTestRequestWithInvalidHttp")
+    @DisplayName("Test invalid http request's header body (invalid scheme) that asserts BadRequest400")
     public void testInvalidHttpSchemeRequestBodyToBeNullInGetRequest(String http) throws IOException {
         assertThrows(com.egehurturk.exceptions.HttpRequestException.class,
                 () -> new HttpRequest(new BufferedReader(new InputStreamReader(prepareIncomingRequestStream(http))))
@@ -240,6 +263,7 @@ public class HttpOnlyGetRequestTest {
     }
 
     @Test
+    @DisplayName("Test null buffered stream for client that is null (asserts BadRequest)")
     public void testNullBufferedStreamException() throws IOException {
         assertThrows(com.egehurturk.exceptions.HttpRequestException.class,
                 () -> new HttpRequest(null)
@@ -247,6 +271,7 @@ public class HttpOnlyGetRequestTest {
     }
 
     @Test
+    @DisplayName("Test null request for client (asserts BadRequest)")
     public void testNullHttpRequest() {
         String nullHttp = " ";
         assertThrows(com.egehurturk.exceptions.HttpRequestException.class,
@@ -255,6 +280,7 @@ public class HttpOnlyGetRequestTest {
     }
 
     @Test
+    @DisplayName("Test invalid header for client (asserts BadRequest)")
     public void testInvalidHeader() {
         String invalidHttp =  "GET / HTTP/1.1\r\n" +
                 "This is invalid\r\n" +
