8359305e1f23a419995b360248ddf0a2b2b9ea25
Ege Hurturk <ege.hurturk@gmail.com>
2021-01-22 18:16:34 +0300

    Created custom URL mapping, however, priorities clash. TODO: Arrange handlers according to their priorities (See todo in DriverClassTest)

--- src/main/java/com/egehurturk/httpd/DriverClassForTest.java
+++ src/main/java/com/egehurturk/httpd/DriverClassForTest.java
@@ -1,10 +1,13 @@
 package com.egehurturk.httpd;
 
 
+import com.egehurturk.handlers.Handler;
 import com.egehurturk.handlers.HttpHandler;
+import com.egehurturk.util.HeaderEnum;
 import com.egehurturk.util.MethodEnum;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class DriverClassForTest {
     public static void main(String[] args) throws IOException {
@@ -13,8 +16,36 @@ public class DriverClassForTest {
         httpServer.configureServer();
         HttpHandler handler = new HttpHandler(httpServer.getConfig());
         // FIXME: DOES NOT WORK ON CUSTOM URL'S
-        String path = new String("/*");
+        String path = "/hello";
+        httpServer.allowCustomUrlMapping(true);
+        httpServer.addHandler(MethodEnum.GET, path, new MyHandler());
         httpServer.addHandler(MethodEnum.GET, path, handler);
+        httpServer.addHandler(MethodEnum.POST, path, handler);
+
         httpServer.start();
     }
+
+    static class MyHandler implements Handler {
+        @Override
+        public HttpResponse handle(HttpRequest request, HttpResponse response) {
+            HttpResponse res = new HttpResponseBuilder().scheme("HTTP/1.1")
+                    .code(200)
+                    .message("OK")
+                    .body("<h1>Hello</h1>".getBytes())
+                    .setStream(new PrintWriter(response.getStream(), false))
+                    .setHeader(HeaderEnum.CONTENT_LENGTH.NAME, ""+("<h1>Hello</h1>".length()))
+                    .setHeader(HeaderEnum.CONTENT_TYPE.NAME, "text/html")
+                    .build();
+            return res;
+        }
+    }
 }
+
+
+/*
+ * FIXME: POST REQUEST IS VERY VERY SLOW?
+ */
+
+/*
+TODO: If a handler is installed with /*, and also another handler with e.g. path /hello is installed, the latter one should have higher priority
+ */
