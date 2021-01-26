9a2282d3e8cad8f86612ba77506cf5890f0010cb
Ege Hurturk <ege.hurturk@gmail.com>
2021-01-26 11:15:36 +0300

    further developed bash file to call jar of the server

--- banzai.sh
+++ banzai.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/zsh
 clear
 clear
 
@@ -16,12 +16,39 @@ echo "
 
 Banzai Server, V1.0 (SNAPSHOT) -
 "
+config="noconfig"
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -p|--port) port="$2"; shift ;;
+        -h|--host) host="$2"; shift ;;
+        -w|--webroot) webroot="$2"; shift ;;
+        -b|--backlog) backlog="$2"; shift ;;
+        -n|--name) name="$2"; shift ;;
+        -c|--config) config="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+echo "========================= SERVER STARTING ========================="
+echo "port: $port"
+echo "host: $host"
+echo "name: $name"
+echo "webroot: $webroot"
+echo "backlog: $backlog"
+
+if [ "$port" = " " ]; then
+  echo "PORT IS EQUAL TO BLANK"
+fi
 
 
+if [ "$config" = "noconfig" ]; then
+  java -jar target/BanzaiServer-1.0-SNAPSHOT-jar-with-dependencies.jar --port $port --host $host --webroot $webroot --name $name --backlog $backlog
+else
+  java -jar target/BanzaiServer-1.0-SNAPSHOT-jar-with-dependencies.jar --config $config
+fi
 
 
-
-#java -jar target/BanzaiServer-1.0-SNAPSHOT-jar-with-dependencies.jar --config src/main/resources/server.properties
 
 
 
