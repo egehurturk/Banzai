b8b5739e9e5be6bba220476d75a8eef1ed605191
Ege Hurturk <ege.hurturk@gmail.com>
2021-02-08 19:15:49 +0300

    New test for FileResponseHandler

--- src/test/java/com/egehurturk/FileResponseTest.java
+++ src/test/java/com/egehurturk/FileResponseTest.java
@@ -1,14 +1,10 @@
 package com.egehurturk;
 
-import com.egehurturk.handlers.FileResponseHandler;
-import com.egehurturk.httpd.HttpRequest;
-import com.egehurturk.httpd.HttpResponse;
 import org.junit.jupiter.api.DisplayName;
-
-import java.io.FileNotFoundException;
 
 @DisplayName("File Response Handler class tests")
 public class FileResponseTest {
+
 
 }
 
