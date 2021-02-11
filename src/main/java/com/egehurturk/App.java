1a58b006bbe832f518bb227ab8119cd91b28945c
Ege Hurturk <ege.hurturk@gmail.com>
2021-02-11 18:50:38 +0300

    .-000

--- src/main/java/com/egehurturk/App.java
+++ src/main/java/com/egehurturk/App.java
@@ -1,17 +1,11 @@
 package com.egehurturk;
-import com.egehurturk.example.TCPServer;
-
-import java.io.IOException;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) throws IOException {
-        TCPServer server1 = new TCPServer(9090);
-       // server1.start();
-
+public class App {
+    public static void main( String[] args ) {
+        System.out.println("Hello, World!");
     }
 }
