3763f6068fff20b5915afaea82f0686584a193c3
Ege Hurturk <ege.hurturk@gmail.com>
2021-02-19 15:50:34 +0300

    Some improvements

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 echo "
        ▄▄▄▄    ▄▄▄       ███▄    █ ▒███████▒ ▄▄▄       ██▓
       ▓█████▄ ▒████▄     ██ ▀█   █ ▒ ▒ ▒ ▄▀░▒████▄    ▓██▒
@@ -26,41 +26,17 @@ ANSI_CYAN="\033[0;36m"
 ANSI_WHITE="\033[1;37m"
 ANSI_YELLOW="\033[1;33m"
 ANSI_NC="\033[0m" # no color
-ANSI_BOLD=$(tput bold)
-NORMAL=$(tput sgr0)
-# Init variables
-BANZAI_SH_L="/usr/local/bin/banzai"
-JAR_FILE_NAME="$(ls ../target | grep "jar-with-dependencies.jar")"
-JAR_LOCATION_SM="/usr/local/bin/${JAR_FILE_NAME}"
+ANSI_BOLD=$(tput bold) # bold
+NORMAL=$(tput sgr0)  # escape bold
 
-function autogenerateproperties() {
-  read AUTO_SERVER_PROP"?  Do you want to generate a server configuration property file automatically? [Y/N] "
-  if [[ $AUTO_SERVER_PROP == "Y" ]] || [[ $AUTO_SERVER_PROP == "y" ]] || [[ $AUTO_SERVER_PROP == "yes" ]]; then
-    cd ..
-    cd src
-    cd main
-    cd resources
-    read NAME"? Enter the name for the .properties file (e.g app.properties): "
-    if [[ -f "$NAME" ]]; then
-       printf "  ${ANSI_RED}${NAME} already exists. ${ANSI_NC}\n"
-       exit 1
-    fi
-    read PORT"? Enter port number for the server to run on (e.g. 8091): "
-    read HOST"? Enter host for the server to run on (e.g. localhost or 0.0.0.0): "
-    SERVERNAME="Banzai"
-    read WEBROOT"? Enter webroot for the server to run on (e.g. www): "
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # full working directory (e.g. here -> /Users/egehurturk/Development/BanzaiServer/scripts)
+BINARY_PATH="/usr/local/bin" # place to store banazi executable
+BANZAI_SH_L="$BINARY_PATH/banzai" # binary banzai path (/usr/local/bin/banzai)
+#FIXME: there won't be any /target dir if i directly clone from github
+JAR_FILE_NAME="$(ls $(dirname $DIR)/target | grep "jar-with-dependencies.jar")" # jar file name (only the name w/ extension)
+JAR_LOCATION_NM="$(dirname $DIR)/target/$JAR_FILE_NAME" # jar file location normal (in the project directory)
+JAR_LOCATION_SM="/opt/banzai/${JAR_FILE_NAME}"
 
-    touch $NAME;
-    {
-      echo "server.port=$PORT"
-      echo "server.host=$HOST"
-      echo "server.name=$SERVERNAME"
-      echo "server.webroot=$WEBROOT"
-    } >> $NAME
-    WORKDIR=$(pwd)
-    printf "  ${ANSI_WHITE}Properties file created in ${WORKDIR}/{$NAME} \n"
-  fi
-}
 
 # trap ctrl-c and call ctrl_c()
 trap ctrl_c INT
@@ -73,34 +49,53 @@ function ctrl_c() {
 
 if [ -L "$BANZAI_SH_L" ]; then
   printf "  ${ANSI_RED}Banzai has been already installed!${ANSI_NC}\n"
-  printf "  ${ANSI_RED}Please delete $BANZAI_SH_L, $JAR_LOCATION_SM to start again.${ANSI_NC}\n"
+  printf "  ${ANSI_RED}Please delete $BANZAI_SH_L to start again.${ANSI_NC}\n"
+  echo ""
+  exit 1
+elif [ -f "$JAR_LOCATION_SM" ]; then
+  printf "  ${ANSI_RED}Banzai has been already installed!${ANSI_NC}\n"
+  printf "  ${ANSI_RED}Please delete $JAR_LOCATION_SM to start again.${ANSI_NC}\n"
+  echo ""
+  exit 1
+elif [ -f "/var/log/banzai" ]; then
+  printf "  ${ANSI_RED}Banzai has been already installed!${ANSI_NC}\n"
+  printf "  ${ANSI_RED}Please delete /var/log/banzai to start again.${ANSI_NC}\n"
   echo ""
   exit 1
 fi
 
 
-read SYMLINK"?  Do you want to create a symbolic link to banzai.sh in ${ANSI_BOLD}/usr/local/bin/banzai?${ANSI_NORMAL} [Y/N] "
+read -p "Creating symbolic link to banzai.sh in ${ANSI_BOLD}/usr/local/bin/banzai. Do you wish to continue?${ANSI_NORMAL} [Y/N] " SYMLINK
 if [[ $SYMLINK == "Y" ]] || [[ $SYMLINK == "y" ]] || [[ $SYMLINK == "yes" ]]; then
   printf "  ${ANSI_CYAN}Creating a symbolic link for banzai.sh to ${ANSI_BOLD}/usr/local/bin/banzai${ANSI_NORMAL}${ANSI_NC}\n"
-  echo "  Working directory: $(pwd)"
   if [[ -L "$BANZAI_SH_L" ]]; then
     printf "  ${ANSI_RED}Banzai is already symlinked${ANSI_NC}\n"
     exit 1
   fi
-  chmod 755 "$(pwd)/banzai.sh"
-  ln -s "$(pwd)/banzai.sh" "/usr/local/bin/banzai"
+  chmod 755 "$DIR/banzai.sh"
+  ln -s "$DIR/banzai.sh" "$BANZAI_SH_L"
   printf "  ${ANSI_GREEN}Symbolic link created!${ANSI_NC}\n"
-  read COPYJAR"?  Do you want to copy the Banzai jar file to ${ANSI_BOLD}/usr/local/bin${ANSI_NORMAL} ? [Y/N] "
-  if [[ $COPYJAR == "Y" ]] || [[ $COPYJAR == "y" ]] || [[ $COPYJAR == "yes" ]]; then
-    printf "  ${ANSI_PURPLE}Copying the jar file to /usr/local/bin/${ANSI_NC}\n"
-    if [[ -f "$JAR_LOCATION_SM" ]]; then
-       printf "  ${ANSI_RED}JAR file already exists!${ANSI_NC}\n"
-       exit 1
-    else
-      sudo cp "../target/$JAR_FILE_NAME" "$JAR_LOCATION_SM"
-      printf "  ${ANSI_GREEN}Done!${ANSI_NC}\n"
-    fi
+
+  printf "  ${ANSI_PURPLE}Copying the jar file to $JAR_LOCATION_SM${ANSI_NC}\n"
+  if [[ -f "$JAR_LOCATION_SM" ]]; then
+     printf "  ${ANSI_RED}JAR file already exists!${ANSI_NC}\n"
+     exit 1
+  else
+    echo "Copying requires sudo (enter machine password) "
+    sudo mkdir "/opt/banzai"
+    sudo cp "$JAR_LOCATION_NM" "$JAR_LOCATION_SM"
+    printf "  ${ANSI_GREEN}Done!${ANSI_NC}\n"
   fi
+
+  printf "  ${ANSI_PURPLE}Creating banzai directory in /var/log/banzai ${ANSI_NC}\n"
+  if [[ -d "/var/log/banzai" ]]; then
+    printf "  ${ANSI_RED}Banzai is already installed${ANSI_NC}\n"
+    exit 1
+  fi
+  echo "Creating log directory requires sudo (enter machine password) "
+  sudo mkdir "/var/log/banzai"
+  printf "  ${ANSI_GREEN}Done!${ANSI_NC}\n"
+
 
 fi
 
@@ -111,8 +106,6 @@ printf "  ${ANSI_CYAN}You can run Banzai server with banzai [options].${ANSI_NC}
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo ""
 
-
-
-
-
-
+# -->> /var/log/banzai: log directory
+# -->> /opt/banzai: banzai jar files
+# -->> /usr/local/bin/banzai --> banzai executable
