c2a98fc3f9f1f20bf8b3c1a7d8b706604c1f6893
Ege Hurturk <ege.hurturk@gmail.com>
2021-05-19 17:28:57 +0300

    Compress the body if it is enabled, if not, use the body directly.

--- src/main/java/com/egehurturk/httpd/HttpResponseBuilder.java
+++ src/main/java/com/egehurturk/httpd/HttpResponseBuilder.java
@@ -2,10 +2,12 @@ package com.egehurturk.httpd;
 
 
 import com.egehurturk.exceptions.HttpResponseException;
+import com.egehurturk.util.Compressor;
 import com.egehurturk.util.Headers;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
 
@@ -56,28 +58,49 @@ public class HttpResponseBuilder {
         try {
             res = HttpResponse.create(this.headers, scheme, code, message, body, stream);
         } catch (HttpResponseException e) {
-            logger.error("Error building Http Response");
+            logger.error("Error building Http Response: ");
             e.printStackTrace();
         }
         return res;
     }
 
     public HttpResponse factory(String scheme, int statusCode, String statusMessage, byte[] body, PrintWriter stream, String mime, String date,
-                                String name, String contentLanguage, int contentLength
+                                String name, String contentLanguage, int contentLength, boolean compressEnabled
                                 ) {
-        HttpResponse response = new HttpResponseBuilder()
-                .scheme(scheme)
-                .code(statusCode)
-                .message(statusMessage)
-                .body(body)
-                .setStream(stream)
-                .setHeader(Headers.CONTENT_TYPE.NAME, mime)
-                .setHeader(Headers.DATE.NAME, date)
-                .setHeader(Headers.SERVER.NAME, name)
-                .setHeader(Headers.CONTENT_LANGUAGE.NAME, contentLanguage)
-                .setHeader(Headers.CONTENT_LENGTH.NAME, ""+contentLength)
-                .build();
-        return response;
+
+        if (compressEnabled) {
+            byte[] compressed_gzip_body = null;
+            try {
+                compressed_gzip_body = Compressor.compress_GZIP(body);
+                return getHttpResponse(scheme, statusCode, statusMessage, compressed_gzip_body, stream, mime, date, name, contentLanguage, compressed_gzip_body.length, "gzip");
+            } catch (IOException e) {
+                logger.error("Something is wrong, just don't panic: ");
+                System.out.println(e.getMessage());
+                return getHttpResponse(scheme, statusCode, statusMessage, body, stream, mime, date, name, contentLanguage, contentLength, null);
+            } catch (IllegalArgumentException e) {
+                logger.error(e.getMessage());
+            }
+            return null;
+        } else {
+            return getHttpResponse(scheme, statusCode, statusMessage, body, stream, mime, date, name, contentLanguage, contentLength, null);
+        }
+
+    }
+
+    private HttpResponse getHttpResponse(String scheme, int statusCode, String statusMessage, byte[] body, PrintWriter stream, String mime, String date, String name, String contentLanguage, int contentLength, String contentEncoding) {
+        HttpResponseBuilder builder = new HttpResponseBuilder()
+            .scheme(scheme)
+            .code(statusCode)
+            .message(statusMessage)
+            .body(body)
+            .setStream(stream)
+            .setHeader(Headers.CONTENT_TYPE.NAME, mime)
+            .setHeader(Headers.DATE.NAME, date)
+            .setHeader(Headers.SERVER.NAME, name)
+            .setHeader(Headers.CONTENT_LANGUAGE.NAME, contentLanguage)
+            .setHeader(Headers.CONTENT_LENGTH.NAME, ""+contentLength)
+            .setHeader(Headers.CONTENT_ENCODING.NAME, contentEncoding);
+        return builder.build();
     }
 
 }
